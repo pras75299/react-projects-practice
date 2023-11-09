10cc110a06fa36158ce2f493869d9d2020d3cf5d
pras75299 <singh.prashantking@gmail.com>
2023-11-09 18:14:41 +0530

    add context in app.jsx

--- 01-contextapi-basic/src/App.jsx
+++ 01-contextapi-basic/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
-  return <div className="text-left">Hello</div>;
+  return (
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
 }
 
 export default App;
