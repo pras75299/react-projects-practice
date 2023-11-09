d37ed0348b467b66cb1ba2abbfb41df2e518e7e7
pras75299 <singh.prashantking@gmail.com>
2023-11-09 18:20:35 +0530

    login component complete

--- 01-contextapi-basic/src/pages/Login.jsx
+++ 01-contextapi-basic/src/pages/Login.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import React, { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUser } = useContext(UserContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+    setUsername("");
+    setPassword("");
+  };
+  return (
+    <div>
+      <h1 className="text-2xl">Login</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+      />{" "}
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button className="text-2xl" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default Login;
