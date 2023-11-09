348ff0996431d74bece2215b448aa9e5d795002e
pras75299 <singh.prashantking@gmail.com>
2023-11-09 18:25:27 +0530

    profile component complete

--- 01-contextapi-basic/src/pages/Profile.jsx
+++ 01-contextapi-basic/src/pages/Profile.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useContext(UserContext);
+  if (!user) return <h1>Please login</h1>;
+
+  return <div>Welcome {user.username}</div>;
 };
 
 export default Profile;
